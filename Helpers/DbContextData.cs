ba7a86da13a0b63e30d0742afdafec28c238997f
Francisco González <fgonzalez@gestionauto.local>
2023-10-23 12:18:21 -0300

    Add New Project

---
+++ Helpers/DbContextData.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace Proyecto_Cliente.Helpers
+{
+    public class DbContextData
+    {
+        private readonly IConfiguration _configuration;
+        public DbContextData(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection Createconnection()
+        {
+
+            return new SqliteConnection(_configuration.GetConnectionString("connectionstr"));
+
+        }
+
+        public async Task Init()
+        {
+            var conn = Createconnection();
+
+            await _initcustomer();
+
+            async Task _initcustomer()
+            {
+                var sqlstr = "CREATE TABLE IF NOT EXISTS Client(Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,Rut TEXT, FirstName TEXT, LastName TEXT, Address TEXT, City TEXT, PhoneNumber TEXT);";
+
+                await conn.ExecuteAsync(sqlstr);
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Proyecto_Cliente.Helpers;
+using Proyecto_Cliente.Services;
+using Proyecto_Cliente.Repositories;
+using System.Text.Json.Serialization;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(j => {
+
+    j.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    j.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+
+});
+
+builder.Services.AddSingleton<DbContextData>();
+builder.Services.AddScoped<Iclient, clientService>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+var app = builder.Build();
+
+{
+    using var appscope = app.Services.CreateScope();
+    var context = appscope.ServiceProvider.GetRequiredService<DbContextData>();
+    await context.Init();
+
+}
+
+//Swagger
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseMiddleware<MiddlewareErrorHandler>();
+app.MapControllers();
+
+app.Run();
